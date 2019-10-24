5681725eca894cffedba1907459d2872b016dafa
Tigran <tigrankoshkelyan@gmail.com>
2019-10-24 22:51:56 +0300

    Minor correction

--- messenger/users/urls.py
+++ messenger/users/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from users.views import *
 
 
-path('profile/', chat_profile, name='chat_profile'),
-path('contacts/', contacts, name='contacts')
+urlpatterns = [
+    path('profile/', chat_profile, name='chat_profile'),
+    path('contacts/', contacts, name='contacts')
+]
+
