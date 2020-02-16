6bd43e361fac0c8acd1fcdc4a4ecb6e6f36a4ab1
Tigran <tigrankoshkelyan@gmail.com>
2020-02-16 18:42:50 +0300

    Selenium test added

--- messenger/application/tests.py
+++ messenger/application/tests.py
@@ -1,5 +1,5 @@
-import json
-from django.test import TestCase, Client
+from django.test import TestCase, Client, LiveServerTestCase
+from selenium import webdriver
 
 
 class FillDataBaseTest(TestCase):
@@ -10,3 +10,35 @@ class FillDataBaseTest(TestCase):
     def test_correct(self):
         response = self.Client.get('/fill_db/')
         self.assertEqual(response.status_code, 200)
+
+
+class SeleniumTests(LiveServerTestCase):
+    selenium = None
+    fixtures = ['fixtures.json']
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.selenium = webdriver.Chrome(executable_path='./chromedriver')
+        cls.selenium.implicitly_wait(30)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
+
+    def test_create_account(self):
+        self.selenium.get('http://localhost:3000/#/')
+        self.selenium.get_screenshot_as_png()      # иначе не кликает, нужно какое-то действие перед кликом
+        self.selenium.find_element_by_xpath('//*[@id="root"]/div/a').click()
+        username_input = self.selenium.find_element_by_xpath('//*[@id="root"]/div/div[2]/div/input[1]')
+        username_input.send_keys('Marianna')
+        tag_input = self.selenium.find_element_by_xpath('//*[@id="root"]/div/div[2]/div/input[2]')
+        tag_input.send_keys('Tangerine')
+        bio_input = self.selenium.find_element_by_xpath('//*[@id="root"]/div/div[2]/div/textarea')
+        bio_input.send_keys('Hey there')
+        password_input = self.selenium.find_element_by_xpath('//*[@id="root"]/div/div[2]/div/div[2]/input[1]')
+        password_input.send_keys('secret')
+        repeat_password_input = self.selenium.find_element_by_xpath('//*[@id="root"]/div/div[2]/div/div[2]/input[2]')
+        repeat_password_input.send_keys('secret')
+        self.selenium.find_element_by_xpath('//*[@id="root"]/div/div[1]/div[3]/div/div/img').click()
